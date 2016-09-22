56a661c0ce1fa470cda7eca32ff1b678e7a6e1bf
Nikhil Ravishankar <nravishankar@vmware.com>
2016-09-22 12:04:42 +0530

    Channel Framework.

--- app/src/main/java/com/mantra/checkin/MainActivity.java
+++ app/src/main/java/com/mantra/checkin/MainActivity.java
@@ -3,6 +3,8 @@ package com.mantra.checkin;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mantra.checkin.NavigationDrawer.NavigationDrawerRecyclerViewAdapter;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView mNavigationDrawerRecyclerView;
+    private RecyclerView.Adapter mNavigationDrawerAdapter;
+    private RecyclerView.LayoutManager mNavigationDrawerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,13 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Initialize Navigation Drawer ListView
+        mNavigationDrawerRecyclerView = (RecyclerView) findViewById(R.id.navigation_drawer_recycler_view);
+        mNavigationDrawerLayoutManager = new LinearLayoutManager(this);
+        mNavigationDrawerRecyclerView.setLayoutManager(mNavigationDrawerLayoutManager);
+        mNavigationDrawerAdapter = new NavigationDrawerRecyclerViewAdapter();
+        mNavigationDrawerRecyclerView.setAdapter(mNavigationDrawerAdapter);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
